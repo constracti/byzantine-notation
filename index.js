866124c8d8af2bcca9d4e7349fb5823223dd30f9
constracti <constracti@gmail.com>
2026-04-17 22:44:33 +0300

    move part list formation to primary
    handle gorgon separately
    add icon

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const color_object = {
 	red: 'bz-red',
 };
 
+/**
+ * @param {string} font
+ * @param {string} char
+ * @returns {HTMLSpanElement}
+ */
+function custom_span(font, char) {
+	const element = document.createElement('span');
+	element.classList.add(font);
+	element.innerHTML = char;
+	return element;
+}
+
 
 class Primary {
 
@@ -69,6 +81,16 @@ class Primary {
 		primary_span.innerHTML = this.char;
 		return primary_span;
 	}
+
+	/**
+	 * @param {MusicContext} music_context
+	 * @param {number} block_index
+	 * @param {Block[]} block_list
+	 * @returns {?Part[]}
+	 */
+	get_parts(music_context, block_index, block_list) {
+		return null;
+	}
 }
 
 
@@ -112,7 +134,7 @@ class PosotitaPrimary extends Primary {
 	 * @returns {boolean}
 	 */
 	starts_with_syneches_elafron() {
-		return this === PosotitaPrimary.syneches_elafron;
+		return this === PosotitaPrimary.syneches_elafron; // TODO also symploki
 	}
 
 	/**
@@ -120,6 +142,103 @@ class PosotitaPrimary extends Primary {
 	 */
 	is_red() {
 		return false;
+	}
+
+	/**
+	 * @param {MusicContext} music_context
+	 * @param {number} block_index
+	 * @param {Block[]} block_list
+	 * @returns {?Part[]}
+	 */
+	get_parts(music_context, block_index, block_list) {
+		const block = block_list[block_index];
+		/**
+		 * @type {Part[]}
+		 */
+		const part_list = [];
+		this.move_list.forEach(move => {
+			music_context.pitch += move;
+			part_list.push({
+				pitch: music_context.pitch,
+				steps: 0,
+				duration: 1,
+				block: block_index,
+			});
+		});
+		// TODO duration from previous gorgon
+		// duration from current syneches elafron
+		if (this.starts_with_syneches_elafron()) {
+			if (part_list.length >= 1) // condition must be true
+				part_list[0].duration -= 1 / 2;
+		}
+		// duration from current gorgon
+		if (block.gorgon !== null) {
+			if (part_list.length >= 2)
+				part_list[part_list.length - 2].duration -= 1 - 1 / block.gorgon.tuplet;
+			if (part_list.length >= 1) // condition must be true
+				part_list[part_list.length - 1].duration -= 1 - 1 / block.gorgon.tuplet;
+			// TODO next parts
+		}
+		// duration from next gorgon or syneches elafron
+		const tuplet = this.#is_followed_by_gorgon(block_index, block_list);
+		if (tuplet !== null) {
+			if (part_list.length >= 1) // condition must be true
+				part_list[part_list.length - 1].duration -= 1 - 1 / tuplet;
+		}
+		// chronos & alloiosi // TODO maybe on secondary subclasses
+		block.secondary_list.forEach(secondary => {
+			switch (secondary.type) {
+				case AlloiosiSecondary.type:
+					/**
+					 * @type {AlloiosiSecondary}
+					 */
+					const alloiosi = secondary;
+					if (part_list.length >= 1) // condition must be true
+						part_list[part_list.length - 1].steps = alloiosi.steps;
+					break;
+				case ChronosSecondary.type:
+					/**
+					 * @type {ChronosSecondary}
+					 */
+					const chronos = secondary;
+					if (part_list.length >= 1) // condition must be true
+						part_list[part_list.length - 1].duration += chronos.beats;
+					break;
+			}
+		});
+		// return
+		return part_list;
+	}
+
+	/**
+	 * @param {number} block_index
+	 * @param {Block[]} block_list
+	 * @returns {?number} tuplet or null
+	 */
+	#is_followed_by_gorgon(block_index, block_list) {
+		while (true) {
+			block_index++;
+			if (block_index === block_list.length)
+				return null;
+			const block = block_list[block_index];
+			switch (block.primary.type) {
+				case PosotitaPrimary.type:
+					/**
+					 * @type {PosotitaPrimary}
+					 */
+					const posotita = block.primary;
+					if (posotita.starts_with_syneches_elafron())
+						return 2;
+					if (posotita.move_list.length >= 2)
+						return null;
+					if (block.gorgon === null)
+						return null;
+					return block.gorgon.tuplet;
+				// TODO siopi
+				default:
+					continue;
+			}
+		}
 	}
 }
 
@@ -309,6 +428,11 @@ class Block {
 	primary;
 
 	/**
+	 * @type {?GorgonSecondary}
+	 */
+	gorgon;
+
+	/**
 	 * @type {Secondary[]}
 	 */
 	secondary_list;
@@ -320,11 +444,13 @@ class Block {
 
 	/**
 	 * @param {Primary} primary
+	 * @param {?GorgonSecondary} gorgon 
 	 * @param {Secondary[]} secondary_list
 	 * @param {?string} text
 	 */
-	constructor(primary, secondary_list, text) {
+	constructor(primary, gorgon, secondary_list, text) {
 		this.primary = primary;
+		this.gorgon = gorgon;
 		this.secondary_list = secondary_list;
 		this.text = text;
 	}
@@ -338,7 +464,13 @@ class Block {
 		const symbol_div = document.createElement('div');
 		symbol_div.classList.add('bz-symbol');
 		block_div.append(symbol_div);
-		symbol_div.append(this.primary.get_span());
+		if (this.primary === PosotitaPrimary.oligon_kentimata && this.gorgon === GorgonSecondary.gorgon) {
+			symbol_div.append(custom_span(font_object.byzantina, 'V'));
+		} else {
+			symbol_div.append(this.primary.get_span());
+			if (this.gorgon !== null)
+				symbol_div.append(this.gorgon.get_span());
+		}
 		symbol_div.append(...this.secondary_list.map(secondary => secondary.get_span()));
 		if (this.text !== null) {
 			const text_div = document.createElement('div');
@@ -353,95 +485,95 @@ class Block {
 
 const block_list = [
 	// Εκ νεότητός μου
-	new Block(PosotitaPrimary.ison, [], 'Εκ'),
-	new Block(PosotitaPrimary.ison, [], 'νε'),
-	new Block(PosotitaPrimary.oligon_kentima_dipla, [], 'ο'),
-	new Block(PosotitaPrimary.oligon, [], 'τη'),
-	new Block(PosotitaPrimary.oligon, [], 'τος'),
-	new Block(PosotitaPrimary.ison, [], 'μου'),
-	new Block(PosotitaPrimary.ison, [], 'ο'),
-	new Block(PosotitaPrimary.ison, [], 'εχ'),
-	new Block(Primary.vareia, [], null),
-	new Block(PosotitaPrimary.apostrofos, [], 'θρο'),
-	new Block(PosotitaPrimary.apostrofos, [], 'ος'),
-	new Block(PosotitaPrimary.oligon, [], 'με'),
-	new Block(PosotitaPrimary.oligon, [], 'πει'),
-	new Block(PosotitaPrimary.oligon_kentimata, [AlloiosiSecondary.yfesi_monogrammi], 'ρα'),
-	new Block(PosotitaPrimary.elafron, [], 'ζει'),
-	new Block(PosotitaPrimary.ison, [], 'ταις'),
-	new Block(PosotitaPrimary.ison, [], 'η'),
-	new Block(PosotitaPrimary.apostrofos, [], 'δο'),
-	new Block(PosotitaPrimary.oligon_kentimata, [AlloiosiSecondary.yfesi_apli, Secondary.psifiston], 'ναις'),
-	new Block(PosotitaPrimary.apostrofos, [], 'φλε'),
-	new Block(PosotitaPrimary.apostrofos, [], 'γει'),
-	new Block(PosotitaPrimary.apostrofos, [], 'με'),
-	new Block(PosotitaPrimary.ison, [], 'ε'),
-	new Block(PosotitaPrimary.ison_petasti, [], 'γω'),
-	new Block(PosotitaPrimary.apostrofos, [], 'δε'),
-	new Block(PosotitaPrimary.oligon, [], 'πε'),
-	new Block(PosotitaPrimary.oligon, [], 'ποι'),
-	new Block(PosotitaPrimary.oligon, [ChronosSecondary.klasma], 'θως'),
-	new Block(PosotitaPrimary.ison, [], 'εν'),
-	new Block(PosotitaPrimary.oligon, [], 'σοι'),
-	new Block(PosotitaPrimary.apostrofos_petasti, [], 'Κυ'),
-	new Block(PosotitaPrimary.apostrofos, [], 'ρι'),
-	new Block(PosotitaPrimary.syneches_elafron, [], 'ε'),
-	new Block(PosotitaPrimary.oligon, [], 'τρο'),
-	new Block(PosotitaPrimary.oligon, [Secondary.psifiston], 'που'),
-	new Block(PosotitaPrimary.apostrofos, [], 'μαι'),
-	new Block(PosotitaPrimary.apostrofos_kentimata, [], 'του'),
-	new Block(PosotitaPrimary.elafron, [ChronosSecondary.klasma], 'τον'),
-	new Block(MartyriaPrimary.martyria_ni, [Secondary.martyriko_simadi_di], null),
+	new Block(PosotitaPrimary.ison, null, [], 'Εκ'),
+	new Block(PosotitaPrimary.ison, null, [], 'νε'),
+	new Block(PosotitaPrimary.oligon_kentima_dipla, null, [], 'ο'),
+	new Block(PosotitaPrimary.oligon, null, [], 'τη'),
+	new Block(PosotitaPrimary.oligon, null, [], 'τος'),
+	new Block(PosotitaPrimary.ison, null, [], 'μου'),
+	new Block(PosotitaPrimary.ison, null, [], 'ο'),
+	new Block(PosotitaPrimary.ison, null, [], 'εχ'),
+	new Block(Primary.vareia, null, [], null),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'θρο'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'ος'),
+	new Block(PosotitaPrimary.oligon, null, [], 'με'),
+	new Block(PosotitaPrimary.oligon, null, [], 'πει'),
+	new Block(PosotitaPrimary.oligon_kentimata, null, [AlloiosiSecondary.yfesi_monogrammi], 'ρα'),
+	new Block(PosotitaPrimary.elafron, null, [], 'ζει'),
+	new Block(PosotitaPrimary.ison, null, [], 'ταις'),
+	new Block(PosotitaPrimary.ison, null, [], 'η'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'δο'),
+	new Block(PosotitaPrimary.oligon_kentimata, null, [AlloiosiSecondary.yfesi_apli, Secondary.psifiston], 'ναις'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'φλε'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'γει'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'με'),
+	new Block(PosotitaPrimary.ison, null, [], 'ε'),
+	new Block(PosotitaPrimary.ison_petasti, null, [], 'γω'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'δε'),
+	new Block(PosotitaPrimary.oligon, null, [], 'πε'),
+	new Block(PosotitaPrimary.oligon, null, [], 'ποι'),
+	new Block(PosotitaPrimary.oligon, null, [ChronosSecondary.klasma], 'θως'),
+	new Block(PosotitaPrimary.ison, null, [], 'εν'),
+	new Block(PosotitaPrimary.oligon, null, [], 'σοι'),
+	new Block(PosotitaPrimary.apostrofos_petasti, null, [], 'Κυ'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'ρι'),
+	new Block(PosotitaPrimary.syneches_elafron, null, [], 'ε'),
+	new Block(PosotitaPrimary.oligon, null, [], 'τρο'),
+	new Block(PosotitaPrimary.oligon, null, [Secondary.psifiston], 'που'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'μαι'),
+	new Block(PosotitaPrimary.apostrofos_kentimata, null, [], 'του'),
+	new Block(PosotitaPrimary.elafron, null, [ChronosSecondary.klasma], 'τον'),
+	new Block(MartyriaPrimary.martyria_ni, null, [Secondary.martyriko_simadi_di], null),
 	// Οι μισούντες Σιών
-	new Block(PosotitaPrimary.oligon_kentima_dipla, [], 'Οι'),
-	new Block(PosotitaPrimary.oligon, [], 'μι'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.petasti, [], 'σου'),
-	new Block(PosotitaPrimary.elafron, [], 'ντες'),
-	new Block(PosotitaPrimary.oligon, [], 'Σι'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.oligon, [ChronosSecondary.klasma], 'ων'),
-	new Block(PosotitaPrimary.ison, [], 'γε'),
-	new Block(PosotitaPrimary.ison, [], 'νη'),
-	new Block(PosotitaPrimary.ison, [], 'θη'),
-	new Block(PosotitaPrimary.kentimata, [], 'η'),
-	new Block(PosotitaPrimary.ison, [], 'τω'),
-	new Block(PosotitaPrimary.elafron, [], 'σαν'),
-	new Block(PosotitaPrimary.oligon, [ChronosSecondary.klasma], 'δη'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.ison, [], 'πριν'),
-	new Block(PosotitaPrimary.ison, [], 'εκ'),
-	new Block(PosotitaPrimary.ison, [], 'σπα'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.elafron_apostrofos, [], 'σθη'),
-	new Block(PosotitaPrimary.kentimata, [], 'η'),
-	new Block(PosotitaPrimary.ison, [], 'ναι'),
-	new Block(PosotitaPrimary.elafron, [], 'ως'),
-	new Block(PosotitaPrimary.oligon_kentimata, [], 'χορ'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.elafron, [ChronosSecondary.klasma], 'τος'),
-	new Block(PosotitaPrimary.oligon, [], 'συγ'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.petasti, [], 'κο'),
-	new Block(PosotitaPrimary.apostrofos, [], 'ψει'),
-	new Block(PosotitaPrimary.oligon, [], 'γαρ'),
-	new Block(PosotitaPrimary.oligon, [], 'Χρι'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.oligon, [ChronosSecondary.klasma], 'στος'),
-	new Block(Primary.stavros, [], null),
-	new Block(PosotitaPrimary.ison, [], 'αυ'),
-	new Block(Primary.diastoli, [], null),
-	new Block(PosotitaPrimary.oligon_kentimata, [AlloiosiSecondary.yfesi_monogrammi, GorgonSecondary.gorgon], 'χε'), // TODO gorgon
-	new Block(PosotitaPrimary.apostrofos, [], 'ε'),
-	new Block(PosotitaPrimary.apostrofos, [], 'νας'),
-	new Block(PosotitaPrimary.ison, [], 'αυ'),
-	new Block(PosotitaPrimary.elafron_apostrofos, [], 'των'),
-	new Block(PosotitaPrimary.oligon, [], 'το'),
-	new Block(PosotitaPrimary.ison, [], 'μη'),
-	new Block(PosotitaPrimary.elafron, [], 'βα'),
-	new Block(PosotitaPrimary.oligon_kentimata, [], 'σα'),
-	new Block(PosotitaPrimary.elafron, [ChronosSecondary.klasma], 'νων'),
-	new Block(MartyriaPrimary.martyria_ni, [Secondary.martyriko_simadi_di], null),
+	new Block(PosotitaPrimary.oligon_kentima_dipla, null, [], 'Οι'),
+	new Block(PosotitaPrimary.oligon, null, [], 'μι'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.petasti, null, [], 'σου'),
+	new Block(PosotitaPrimary.elafron, null, [], 'ντες'),
+	new Block(PosotitaPrimary.oligon, null, [], 'Σι'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.oligon, null, [ChronosSecondary.klasma], 'ων'),
+	new Block(PosotitaPrimary.ison, null, [], 'γε'),
+	new Block(PosotitaPrimary.ison, null, [], 'νη'),
+	new Block(PosotitaPrimary.ison, null, [], 'θη'),
+	new Block(PosotitaPrimary.kentimata, null, [], 'η'),
+	new Block(PosotitaPrimary.ison, null, [], 'τω'),
+	new Block(PosotitaPrimary.elafron, null, [], 'σαν'),
+	new Block(PosotitaPrimary.oligon, null, [ChronosSecondary.klasma], 'δη'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.ison, null, [], 'πριν'),
+	new Block(PosotitaPrimary.ison, null, [], 'εκ'),
+	new Block(PosotitaPrimary.ison, null, [], 'σπα'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.elafron_apostrofos, null, [], 'σθη'),
+	new Block(PosotitaPrimary.kentimata, null, [], 'η'),
+	new Block(PosotitaPrimary.ison, null, [], 'ναι'),
+	new Block(PosotitaPrimary.elafron, null, [], 'ως'),
+	new Block(PosotitaPrimary.oligon_kentimata, null, [], 'χορ'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.elafron, null, [ChronosSecondary.klasma], 'τος'),
+	new Block(PosotitaPrimary.oligon, null, [], 'συγ'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.petasti, null, [], 'κο'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'ψει'),
+	new Block(PosotitaPrimary.oligon, null, [], 'γαρ'),
+	new Block(PosotitaPrimary.oligon, null, [], 'Χρι'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.oligon, null, [ChronosSecondary.klasma], 'στος'),
+	new Block(Primary.stavros, null, [], null),
+	new Block(PosotitaPrimary.ison, null, [], 'αυ'),
+	new Block(Primary.diastoli, null, [], null),
+	new Block(PosotitaPrimary.oligon_kentimata, GorgonSecondary.gorgon, [AlloiosiSecondary.yfesi_monogrammi], 'χε'), // TODO gorgon
+	new Block(PosotitaPrimary.apostrofos, null, [], 'ε'),
+	new Block(PosotitaPrimary.apostrofos, null, [], 'νας'),
+	new Block(PosotitaPrimary.ison, null, [], 'αυ'),
+	new Block(PosotitaPrimary.elafron_apostrofos, null, [], 'των'),
+	new Block(PosotitaPrimary.oligon, null, [], 'το'),
+	new Block(PosotitaPrimary.ison, null, [], 'μη'),
+	new Block(PosotitaPrimary.elafron, null, [], 'βα'),
+	new Block(PosotitaPrimary.oligon_kentimata, null, [], 'σα'),
+	new Block(PosotitaPrimary.elafron, null, [ChronosSecondary.klasma], 'νων'),
+	new Block(MartyriaPrimary.martyria_ni, null, [Secondary.martyriko_simadi_di], null),
 ];
 
 /**
@@ -455,72 +587,33 @@ const block_list = [
 
 /**
  * map block index to part index
- * @type {Map<number, Part}
+ * @type {Map<number, number}
  */
 const part_map = new Map();
 
 /**
- * @param {Block[]} block_list
- * @param {number} block_index
+ * @typedef MusicContext TODO include scale
+ * @type {object}
+ * @property {number} pitch
  */
-function block_sees_syneches_elafron(block_list, block_index) {
-	while (true) {
-		block_index++;
-		if (block_index === block_list.length)
-			return false;
-		const block = block_list[block_index];
-		if (block.primary.type !== PosotitaPrimary.type)
-			continue;
-		/**
-		 * @type {PosotitaPrimary}
-		 */
-		const primary = block.primary;
-		return primary.starts_with_syneches_elafron();
-	}
-}
+
+/**
+ * @type {MusicContext}
+ */
+const music_context = {
+	pitch: 0,
+};
 
 /**
  * @type {Part[]}
  */
 const part_list = [];
 block_list.forEach((block, block_index, block_list) => {
-	switch (block.primary.type) {
-		case PosotitaPrimary.type:
-			/**
-			 * @type {PosotitaPrimary}
-			 */
-			const primary = block.primary;
-			primary.move_list.forEach((move, move_index, move_list) => {
-				part_list.push({
-					pitch: (part_list.length > 0 ? part_list[part_list.length - 1].pitch : 0) + move,
-					steps: 0,
-					duration: primary.starts_with_syneches_elafron() && move_index === 0 || block_sees_syneches_elafron(block_list, block_index) && move_index === move_list.length - 1 ? 0.5 : 1,
-					block: block_index,
-				});
-				if (!part_map.has(block_index))
-					part_map.set(block_index, part_list.length - 1);
-			});
-			break;
-		// TODO siopi
-	}
-	block.secondary_list.forEach(secondary => {
-		switch (secondary.type) {
-			case AlloiosiSecondary.type:
-				/**
-				 * @type {AlloiosiSecondary}
-				 */
-				const alloiosi = secondary;
-				part_list[part_list.length - 1].steps = alloiosi.steps;
-				break;
-			case ChronosSecondary.type:
-				/**
-				 * @type {ChronosSecondary}
-				 */
-				const chronos = secondary;
-				part_list[part_list.length - 1].duration += chronos.beats;
-				break;
-		}
-	});
+	const part_list_of_block = block.primary.get_parts(music_context, block_index, block_list);
+	if (part_list_of_block === null)
+		return;
+	part_map.set(block_index, part_list.length);
+	part_list.push(...part_list_of_block);
 });
 
 /**
@@ -546,7 +639,7 @@ if (scale.reduce((acc, cur) => acc + cur, 0) !== 72)
 	throw new Error('scale sum is not 72');
 const partial = scale.map((cur, ind, arr) => arr.slice(0, ind).reduce((acc, cur) => acc + cur, 0));
 
-const audioContext = new AudioContext();
+const audio_context = new AudioContext();
 
 /**
  * @param {number} index part
@@ -557,17 +650,17 @@ function play(index) {
 	const part = part_list[index];
 	const octave = Math.floor(part.pitch / 7);
 	const note = part.pitch - octave * 7;
-	const oscillatorNode = new OscillatorNode(audioContext, {
+	const oscillator_node = new OscillatorNode(audio_context, {
 		frequency: 440 * Math.pow(2, octave + (partial[note] - partial[5] + part.steps) / 72),
 		type: 'triangle', // TODO simulate musical instrument
 	});
 	const block_div = container_div.children[part.block];
 	block_div.classList.add('bz-active');
-	oscillatorNode.connect(audioContext.destination);
-	oscillatorNode.start();
+	oscillator_node.connect(audio_context.destination);
+	oscillator_node.start();
 	setTimeout(() => {
 		block_div.classList.remove('bz-active');
-		oscillatorNode.stop();
+		oscillator_node.stop();
 		play(index + 1);
 	}, 300 * part.duration);
 }
